5398922d900bbe38a973a02fc71a2faad37d4fa4
Paul Houghton <paul4hough@gmail.com>
2013-10-13 12:48:54 -0600

    Need devel branch for now

--- setup-product.bash
+++ setup-product.bash
@@ -6,5 +6,7 @@
 # Copyright (c) 2013 Paul Houghton <paul4hough@gmail.com>
 #
 git clone https://github.com/pahoughton/product-setup _setup_temp
+cd _setup_temp
+git checkout devel
 bash _setup_temp/product-setup.bash
 
