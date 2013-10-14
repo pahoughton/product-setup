68ae9492dbd75dfa49dee9f206d3449bd887cc11
Paul Houghton <paul4hough@gmail.com>
2013-10-14 05:34:04 -0600

    opps wrong dir

--- setup-product.bash
+++ setup-product.bash
@@ -6,7 +6,8 @@
 # Copyright (c) 2013 Paul Houghton <paul4hough@gmail.com>
 #
 git clone https://github.com/pahoughton/product-setup _setup_temp
-cd _setup_temp
+pushd _setup_temp
 git checkout devel
+popd
 bash _setup_temp/product-setup.bash
 
