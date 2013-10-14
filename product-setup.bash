efa7604530d0ed92cc7ef150730cf23b700bf887
Paul Houghton <paul4hough@gmail.com>
2013-10-14 11:18:56 -0600

    Use devel branch of install-puppet for now.

--- product-setup.bash
+++ product-setup.bash
@@ -24,7 +24,11 @@ pushd "${mydir}"
 ppver=`puppet --version`
 if [ "${ppver}" < "3.2.0" ; then
     # need to install
+    # FIXME - change to clone specific version
     git clone https://github.com/pahoughton/install-puppet
+    pushd install-puppet >/dev/null
+    git checkout devel
+    popd > /dev/null
     python install-puppet/bin/install-puppet.py || exit 1
 fi
 
