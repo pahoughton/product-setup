fceeea6780f49529e6e988cc9a49f2dfc5ff12c1
Paul Houghton <paul4hough@gmail.com>
2013-10-16 06:21:31 -0600

    Test with lastest install-puppet

--- setup-product.bash
+++ setup-product.bash
@@ -6,11 +6,14 @@
 # Copyright (c) 2013 Paul Houghton <paul4hough@gmail.com>
 #
 [ -n "${DEBUG}" ] && set -x
+cat >> ~/.ssh/config <<EOF
+Host heroku.com
+    StrictHostKeyChecking no
+Host github.com
+    StrictHostKeyChecking no
+EOF
 
 [ -d _setup_temp ] || git clone https://github.com/pahoughton/product-setup _setup_temp
-pushd _setup_temp
-git checkout devel
-popd
 pwd
 ls -l
 DEBUG="${DEBUG}" bash _setup_temp/product-setup.bash
